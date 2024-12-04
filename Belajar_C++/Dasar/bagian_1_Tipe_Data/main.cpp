267f0e71865faa01e6b0a8cb4d5f0fff6f670f17
Rabbani <87192370+FathurRabbani@users.noreply.github.com>
2024-12-04 09:59:00 +0700

    memindahkan dan menambahkan ke github

---
+++ Belajar_C++/Dasar/bagian_1_Tipe_Data/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(){
+    // tipe data c++
+
+    // tipe integer
+    char tipeChar;  // tipe char memiliki ukuran memori 1 byte nilai minimum -127 dan maksimum +127
+    unsigned char tipeCharU; // memiliki ukuran memori 1 byte nilai minimum 0 dan maksimum 255
+
+    // tipe short memiliki ukuran memori 2 byte
+    short tipeShort; // nilai bilangan bulat minimum -32.768 dan maksimum +32.768
+    unsigned short tipeShort; // memiliki nilai bilangan bulat minimum 0 dan maksimum + 65.535
+
+    // tipe int
+    int tipeInt; // ukuran memori 4 byte dan nilai bilangan bulat antara -2 miliar dan +2 miliar
+    unsigned int tipeIntU; // ukuran memori nilai 4 byte nilai bilangan bulat min 0 dan maks 2 miliar
+
+    // tipe long memiliki ukuran 8 byte
+    long tipeLong; // nilai bilangan bulat minimal -9 kuintiliun dan maksimum +9 kuintiliun
+    unsigned long tipeLongU; // nilai  bilangan bulat minimal 0 maksimum 18 kuintiliun
+
+    // tipe floating point
+    float tipeF; // float memiliki ukuran 4 byte dan nilai minimum -3.4e38 dan maksimum +3.4e38
+    double tipeD; // double memiliki ukuran 8 byte dan nilai minimum -1.7e308 dan maksimum +1.7e308
+    long double tipeDoubleL; // long double memiliki ukuran 16 byte dan nilai minimum -1.1e4932 dan maksimum +1.1e4932
+
+    // tipe special
+    wchar_t tipeWchar; 
+    
+}
